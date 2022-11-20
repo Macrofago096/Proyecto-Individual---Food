8956ee5cc119462c734685330b7ed217be400e44
unknown <Macrofago096>
2022-11-19 21:27:06 -0300

    conf

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import LandingPage from './React/LandingPage/LandingPage';
+import Home from './React/Home/Home';
+import RecipeCreate from './React/RecipeCreate/RecipeCreate';
+import Detail from './React/Detail/Detail';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Food</h1>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route exact path='/' component={LandingPage}/>
+          <Route>
+            <Route className="home-conteiner" path='/home' component={Home}/>
+            <Route path='/recipe' component={RecipeCreate}/>
+            <Route path='/detail/:id' component={Detail}/>
+          </Route>
+        </Switch> 
+      </div>
+    </BrowserRouter>
   );
 }
 
